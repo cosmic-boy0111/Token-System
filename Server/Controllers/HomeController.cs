74adcd80dbbaabeb1be9f9def548c0945551a8a1
cosmic-boy0111 <87473128+cosmic-boy0111@users.noreply.github.com>
2023-03-22 12:09:10 +0530

    Add project files.

---
+++ Server/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace Server.Controllers
+{
+    public class HomeController : ApiController
+    {
+        // GET: Home
+        public class Greet
+        {
+            public string Name { get; set; }
+        }
+
+        public IHttpActionResult getMessage()
+        {
+            return Ok( new Greet() { Name = "Hello world" } );
+        }
+
+    }
+}
